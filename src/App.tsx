5c41bbd4c68735eed673c27b1bd67d0a2686e1ea
Ahmet-BAYIKLILAR <ahmet.bayiklilar@gmail.com>
2025-06-04 18:07:34 +0300

    fix: React Router kaldırıldı ve Next.js yapısına geçildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ReviewPage from './pages/ReviewPage';
-import ReviewSection from './components/ReviewSection';
+import React from 'react';
+import type { AppProps } from 'next/app';
 
-const App: React.FC = () => {
-  const [showReviews, setShowReviews] = useState(false);
-
-  return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/reviews" element={<ReviewPage />} />
-        </Routes>
-        <ReviewSection isOpen={showReviews} onClose={() => setShowReviews(false)} />
-      </div>
-    </Router>
-  );
-};
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
 
 export default App; 
