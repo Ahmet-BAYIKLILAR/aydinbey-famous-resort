c2701717980021193a32eb4a685203f18adf3c45
Ahmet-BAYIKLILAR <ahmet.bayiklilar@gmail.com>
2025-06-04 17:56:06 +0300

    fix: Next.js ve Netlify yapılandırmaları optimize edildi

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  swcMinify: true,
+  poweredByHeader: false,
 }
 
 module.exports = nextConfig 
